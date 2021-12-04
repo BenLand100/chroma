33da401625bb5b6b7a58e06b368d7acf2c345bfb
Nicholas Ruof <nickruof@uw.edu>
2021-12-03 20:57:12 -0800

    Nruof/plot db (#3)
    * Plot db entries
    
    * Finish plotting
    
    * Add spaces
    
    * plotting changes
    
    * Working db plotter

---
+++ chroma/exp_tools/database/plotting.py
@@ -0,0 +1,38 @@
+import os
+import matplotlib.pyplot as plt
+
+def plot_1D(ax, value, label):
+    ax.bar(value, height=1, width=0.01*value)
+    ax.set_xlabel(label)
+
+
+def plot_2D(ax, x, y, xlabel, ylabel):
+    ax.plot(x, y)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+
+
+def db_visualizer(database, save_name=None, save_path=None, figsize=None):
+    all_entries = database.all()
+    for entry in all_entries:
+        keys = list(entry.keys())
+        fig, ax = plt.subplots(len(keys)-1, 1, figsize=figsize)
+        title = entry["name"]
+        fig.suptitle(entry["name"])
+        for i, key in enumerate(keys):
+            if key == "name":
+                continue
+            else:
+                key_data = entry[key]
+                if isinstance(key_data, float) | isinstance(key_data, int):
+                    plot_1D(ax[i-1], key_data, key)
+                elif isinstance(key_data, list):
+                    if isinstance(key_data[0][0], float):
+                        plot_2D(ax[i-1], key_data[1], key_data[0], "Wavelength (nm)", key)
+                    else:
+                        for data in key_data:
+                            plot_2D(ax[i-1], data[1], data[0], "Wavelength (nm)", key)
+        plt.tight_layout()
+        if save_path:
+            destination = os.path.join(save_path, f"{title}_{save_name}.png")
+            plt.savefig(destination)
